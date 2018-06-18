b4ad6c8f1c9658f2000d88440a7b2aa6c2f70420
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2018-06-18 17:14:34 -0300

    Added integration tests.

--- test/express.test.js
+++ test/express.test.js
@@ -1,3 +1,9 @@
+import express, { Router } from 'express'
+import request from 'supertest'
+import bodyParser from 'body-parser'
+import { graphqlExpress } from 'apollo-server-express'
+import { makeExecutableSchema } from 'graphql-tools'
+
 import persistedQuery from 'graphql-apq/express'
 
 describe('express middleware', () => {
@@ -103,6 +109,141 @@ describe('express middleware', () => {
       expect(cache.get).toHaveBeenCalledTimes(1)
       expect(cache.get).toHaveBeenCalledWith(sha256Hash)
       expect(request.body).toHaveProperty('query', query)
+    })
+  })
+
+  describe('integration', () => {
+    let app
+
+    beforeEach(() => {
+      const typeDefs = `
+        type SomeType {
+          field: String
+        }
+
+        type Query {
+          someType: SomeType
+        }
+
+        schema {
+          query: Query
+        }
+      `
+
+      const resolvers = {
+        Query: {
+          someType: () => ({})
+        },
+        SomeType: {
+          field: () => 'field value'
+        }
+      }
+
+      const schema = makeExecutableSchema({ typeDefs, resolvers })
+
+      const graphql = new Router()
+        .use(bodyParser.json())
+        .use(persistedQuery())
+        .use(
+          graphqlExpress({
+            schema
+          })
+        )
+
+      app = express().use('/graphql', graphql)
+    })
+
+    const operationName = 'SomeOperation'
+
+    const query = `
+        query ${operationName} {
+          someType {
+            field
+          }
+        }
+      `
+
+    const sha256Hash = 'some-hash'
+    const extensions = { persistedQuery: { sha256Hash } }
+
+    const promisifyRequest = req =>
+      new Promise((resolve, reject) =>
+        req.end((err, res) => (err ? reject(err) : resolve(res)))
+      )
+
+    const requests = {
+      query: () =>
+        promisifyRequest(
+          request(app)
+            .post('/graphql')
+            .send({
+              operationName,
+              query
+            })
+        ),
+      hash: () =>
+        promisifyRequest(
+          request(app)
+            .post('/graphql')
+            .send({
+              operationName,
+              extensions
+            })
+        ),
+      hashAndQuery: () =>
+        promisifyRequest(
+          request(app)
+            .post('/graphql')
+            .send({
+              operationName,
+              extensions,
+              query
+            })
+        )
+    }
+
+    it('should respond default query based queries', async () => {
+      const response = await requests.query()
+      expect(response.body).toHaveProperty('data.someType.field', 'field value')
+    })
+
+    it('should respond with not found response when hash is given but no query is found', async () => {
+      const response = await requests.hash()
+      expect(response.body).toHaveProperty(
+        'errors.0.message',
+        'PersistedQueryNotFound'
+      )
+    })
+
+    it('should respond when both hash and query are provided', async () => {
+      const response = await requests.hashAndQuery()
+      expect(response.body).toHaveProperty('data.someType.field', 'field value')
+    })
+
+    it('should cache hash based request and respond subsequent calls correctly', async () => {
+      let full
+      let hash
+
+      hash = await requests.hash()
+      expect(hash.body).not.toHaveProperty('data.someType.field', 'field value')
+      expect(hash.body).toHaveProperty(
+        'errors.0.message',
+        'PersistedQueryNotFound'
+      )
+
+      full = await requests.hashAndQuery()
+      expect(full.body).toHaveProperty('data.someType.field', 'field value')
+      expect(full.body).not.toHaveProperty(
+        'errors.0.message',
+        'PersistedQueryNotFound'
+      )
+
+      hash = await requests.hash()
+      expect(hash.body).toHaveProperty('data.someType.field', 'field value')
+      expect(hash.body).not.toHaveProperty(
+        'errors.0.message',
+        'PersistedQueryNotFound'
+      )
     })
   })
 })
